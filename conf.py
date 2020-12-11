853a9ea42df2e8a2bcf34649815f5d4f9ea440bd
John Bieling <john.bieling@gmx.de>
2020-12-11 13:52:54 +0100

    Improve usability
    - use sphinx_rtd_theme to be able to browse sidemenu without re-loading page
    - add all usable Firefox APIs
    - use tables for Thunderbird and Firefox APIs to include descriptions
    - use captions in sidebar for better readability
    - do not include changelog on main page, only in sidebar
    - add tutorial for event listeners

--- conf.py
+++ conf.py
@@ -2,3 +2,22 @@ project = u'Thunderbird WebExtension APIs'
 source_suffix = '.rst'
 master_doc = 'index'
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', 'overlay']
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+def setup(app):
+   #app.add_javascript("custom.js")
+   app.add_stylesheet("theme_overrides.css")
