0d616ccf4ee2fd138eeec7fffcd02a6ca342418e
John Bieling <john.bieling@gmx.de>
2020-12-14 13:35:28 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -18,18 +18,18 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-def setup(app):
-   #app.add_javascript("custom.js")
-   app.add_stylesheet("theme_overrides.css")
-
-versionwarning_default_message = "This page is part of the API documentation of the no longer supported Thunderbird READTHEDOCS_VERSION. The current version of this documentation is for Thunderbird {newest}."
 
 versionwarning_messages = {
-    'latest': 'This is a custom message only for version "latest" of this documentation.',
+    'latest': 'This documentation is for pre-release versions of Thunderbird. See the “stable” version of this documentation for the current ESR release of Thunderbird.',
 }
+versionwarning_default_message = "This documentation is for Thunderbird READTHEDOCS_VERSION, which is no longer supported. See the “stable” version for the current ESR release of Thunderbird."
+versionwarning_body_selector = 'div[itemprop="articleBody"]'
 
 extensions = [
      # ... other extensions here
      'versionwarning.extension',
 ]
 
+def setup(app):
+   #app.add_javascript("custom.js")
+   app.add_stylesheet("theme_overrides.css")
