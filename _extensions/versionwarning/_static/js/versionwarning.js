322ca18bff2ccf6ff2d338de5780a30ea9fadc5e
John Bieling <john.bieling@gmx.de>
2020-12-15 09:42:28 +0100

    Update versionwarning.js

--- _extensions/versionwarning/_static/js/versionwarning.js
+++ _extensions/versionwarning/_static/js/versionwarning.js
@@ -4,8 +4,8 @@ function injectVersionWarningBanner(running_version, version, config) {
     console.debug("injectVersionWarningBanner");
     var version_url = window.location.pathname.replace(running_version.slug, version.slug);
     var warning = $(config.banner.html);
-    console.debug(warning);
-    console.debug(warning.replace("{message}", $(config.banner.current)));
+    console.debug("" + config.banner.html);
+    console.debug("" + config.banner.html.replace("{message}", config.banner.current)));
 
     warning
       .find("a")
