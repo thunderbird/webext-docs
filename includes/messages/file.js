f459929540ff7e5f5753cf39c181fc2f748dc84b
e-gaulue <edouard@e-gaulue.com>
2021-05-18 09:21:57 +0200

    Why message.id?
    Don't have the context, but according to me it's messageId in the getAttachmentFile() call.
    
    Regards,

--- includes/messages/file.js
+++ includes/messages/file.js
@@ -1,7 +1,7 @@
 let attachments = await browser.messages.listAttachments(messageId);
 for (let att of attachments) {
     let file = await browser.messages.getAttachmentFile(
-        message.id,
+        messageId,
         att.partName
     );
     let content = await file.text();
