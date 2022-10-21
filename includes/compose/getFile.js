233b677d71eabd10568923c320dc442cc540c345
John Bieling <john.bieling@gmx.de>
2022-10-21 17:40:08 +0200

    Update getFile.js

--- includes/compose/getFile.js
+++ includes/compose/getFile.js
@@ -1,6 +1,6 @@
 /**
  * Backward-compatible drop-in replacement for the deprecated
- * ComposeAttachment.getFile() function. Instead calling
+ * ComposeAttachment.getFile() function. Instead of calling
  * attachment.getFile(), call getFile(attachment).
  */
 function getFile(attachment) {
