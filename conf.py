736d849567e2ed57b34a97fb3148a6e446b0bd7a
John Bieling <john.bieling@gmx.de>
2022-10-23 23:59:23 +0200

    unstick navigation

--- conf.py
+++ conf.py
@@ -12,7 +12,7 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     # Toc options
     'collapse_navigation': False,
-    'sticky_navigation': True,
+    'sticky_navigation': False,
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False
