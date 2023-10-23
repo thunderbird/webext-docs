d2bbc4f4d8c7edc2c4f52744a271dfb2c49d5f77
John Bieling <john.bieling@gmx.de>
2023-10-23 14:50:30 +0200

    fix theme

--- conf.py
+++ conf.py
@@ -7,8 +7,17 @@ project = u'WebExtension API Documentation for Thunderbird Beta<br><br>Manifest 
 source_suffix = '.rst'
 master_doc = 'index'
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', 'overlay']
-html_theme = 'sphinx_rtd_theme'
-#html_theme_path = ['_themes']
+
+extensions = [
+    # ... other extensions here
+    'versionwarning.extension',
+    'apiheader',
+    'apimember',
+    'apisectionannotationhack',
+    'sphinx_rtd_theme',
+]
+
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     # Toc options
     'collapse_navigation': False,
@@ -42,15 +51,6 @@ versionwarning_current_indexmessage = 'This is the API documentation for the cur
 versionwarning_older_type = 'warning'
 versionwarning_older_title = 'Warning'
 versionwarning_older_message = 'This is an outdated API documentation for Thunderbird {this}. See version {newest} for the current ESR of Thunderbird.'
-
-
-extensions = [
-    # ... other extensions here
-    'versionwarning.extension',
-    'apiheader',
-    'apimember',
-    'apisectionannotationhack',
-]
 
 def setup(app):
    #app.add_javascript("custom.js")
